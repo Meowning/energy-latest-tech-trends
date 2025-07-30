f23cee0c5d6ad3a7a4c31224fffe26f46be65a63
Meowning <118808242+Meowning@users.noreply.github.com>
2025-07-30 08:50:44 +0000

    initial settings

---
+++ backend/main.go
@@ -0,0 +1,17 @@
+// myapp/backend/main.go
+
+package main
+
+import (
+    "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, Fiber!")
+    })
+
+    app.Listen(":3000")
+}
